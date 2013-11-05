082e635b29dc12fc7697e94e9a0af1751994f7ed
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2013-11-05 21:08:14 +0100

    Added angular to the sortiment page aswell,

---
+++ sortiment/controllers.js
@@ -0,0 +1,71 @@
+var sortimentApp = angular.module('sortimentApp', []);
+
+sortimentApp.controller('sortimentCtrl', function ($scope) {
+    $scope.categories = [{
+        'name': "Möbler för utomhusbruk",
+        'link': "mobler.html"
+    }, {
+        'name': "Äta",
+        'link': ""
+    }, {
+        'name': "Skrivbord",
+        'link': ""
+    }, {
+        'name': "Speglar",
+        'link': ""
+    }, {
+        'name': "Barnens IKEA",
+        'link': ""
+    }, {
+        'name': "Tillaga",
+        'link': ""
+    }, {
+        'name': "Badrumsförvaring",
+        'link': ""
+    }, {
+        'name': "Sängar & madrasser",
+        'link': ""
+    }, {
+        'name': "Stolar",
+        'link': ""
+    }, {
+        'name': "Klädförvaring",
+        'link': ""
+    }, {
+        'name': "Dekoration",
+        'link': ""
+    }, {
+        'name': "Golv",
+        'link': ""
+    }, {
+        'name': "IKEA FAMILY produkter",
+        'link': ""
+    }, {
+        'name': "Köksskåp & vitvaror",
+        'link': ""
+    }, {
+        'name': "Belysning",
+        'link': ""
+    }, {
+        'name': "Småförvaring",
+        'link': ""
+    }, {
+        'name': "Soffor & fåtöljer",
+        'link': ""
+    }, {
+        'name': "Förvaringsmöbler",
+        'link': ""
+    }, {
+        'name': "Bord",
+        'link': ""
+    }, {
+        'name': "Textil & Mattor",
+        'link': ""
+    }, {
+        'name': "Verktyg & beslag",
+        'link': ""
+    }, {
+        'name': "TV & mediaförvaring",
+        'link': ""
+    }];
+});
--- varuhus/controllers.js
+++ varuhus/controllers.js
@@ -1,1 +1,38 @@
 var warehouseApp = angular.module('warehouseApp', []);
+
+warehouseApp.controller('warehouseCtrl', function($scope){
+	$scope.stores = [
+	{'img': '',
+	'location':"Borlänge"},
+	{'img': '',
+	'location':"Göteborg Kållered"},
+	{'img': '',
+	'location':"Haparanda Tornio"},
+	{'img': '',
+	'location':"Jönköping"},
+	{'img': '',
+	'location':"Malmö"},
+	{'img': '',
+	'location':"Stockholm Kungens kurva"},
+	{'img': '',
+	'location':"Älmhult"},
+	{'img': '',
+	'location':"Gävle"},
+	{'img': '',
+	'location':"Göteborg Bäckebol"},
+	{'img': '',
+	'location':"Helsingborg"},
+	{'img': '',
+	'location':"Kalmar"},
+	{'img': '',
+	'location':"Linköping"},
+	{'img': '',
+	'location':"Stockholm Barkarby"},
+	{'img': '',
+	'location':"Sundsvall"},
+	{'img': '',
+	'location':"Uddevalla"},
+	{'img': '',
+	'location':"Västerås"}
+	];
+});
