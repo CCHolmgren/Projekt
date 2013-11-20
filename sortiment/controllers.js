fe5ff3adf09713b37df7d1e60e32aa86075ae4d0
Christfoffer Holmgren <christoffer.holmgren@gmail.com>
2013-11-20 09:36:50 +0100

    Just a bit more tinkering

--- sortiment/controllers.js
+++ sortiment/controllers.js
@@ -5,111 +5,111 @@ sortimentApp.controller('sortimentCtrl', function ($scope) {
         'name': "Möbler för utomhusbruk",
         'link': "mobler.html",
         'imgalt': "Beskrivning för kategorin",
-        'imgsrc': ""
+        'img': "http://www.ikea.com/se/sv/media/categories/seo_image/Sovrumsf%C3%B6rvaring%20fr%C3%A5n%20IKEA__bedroom_storage_340.jpg"
     }, {
         'name': "Äta",
         'link': "",
         'imgalt': "Beskrivning för kategorin",
-        'imgsrc': ""
+        'img': "http://www.ikea.com/se/sv/media/categories/seo_image/Sovrumsf%C3%B6rvaring%20fr%C3%A5n%20IKEA__bedroom_storage_340.jpg"
     }, {
         'name': "Skrivbord",
         'link': "",
         'imgalt': "Beskrivning för kategorin",
-        'imgsrc': ""
+        'img': "http://www.ikea.com/se/sv/media/categories/seo_image/Sovrumsf%C3%B6rvaring%20fr%C3%A5n%20IKEA__bedroom_storage_340.jpg"
     }, {
         'name': "Speglar",
         'link': "",
         'imgalt': "Beskrivning för kategorin",
-        'imgsrc': ""
+        'img': "http://www.ikea.com/se/sv/media/categories/seo_image/Sovrumsf%C3%B6rvaring%20fr%C3%A5n%20IKEA__bedroom_storage_340.jpg"
     }, {
         'name': "Barnens IKEA",
         'link': "",
         'imgalt': "Beskrivning för kategorin",
-        'imgsrc': ""
+        'img': "http://www.ikea.com/se/sv/media/categories/seo_image/Sovrumsf%C3%B6rvaring%20fr%C3%A5n%20IKEA__bedroom_storage_340.jpg"
     }, {
         'name': "Tillaga",
         'link': "",
         'imgalt': "Beskrivning för kategorin",
-        'imgsrc': ""
+        'img': "http://www.ikea.com/se/sv/media/categories/seo_image/Sovrumsf%C3%B6rvaring%20fr%C3%A5n%20IKEA__bedroom_storage_340.jpg"
     }, {
         'name': "Badrumsförvaring",
         'link': "",
         'imgalt': "Beskrivning för kategorin",
-        'imgsrc': ""
+        'img': "http://www.ikea.com/se/sv/media/categories/seo_image/Sovrumsf%C3%B6rvaring%20fr%C3%A5n%20IKEA__bedroom_storage_340.jpg"
     }, {
         'name': "Sängar & madrasser",
         'link': "",
         'imgalt': "Beskrivning för kategorin",
-        'imgsrc': ""
+        'img': "http://www.ikea.com/se/sv/media/categories/seo_image/Sovrumsf%C3%B6rvaring%20fr%C3%A5n%20IKEA__bedroom_storage_340.jpg"
     }, {
         'name': "Stolar",
         'link': "",
         'imgalt': "Beskrivning för kategorin",
-        'imgsrc': ""
+        'img': "http://www.ikea.com/se/sv/media/categories/seo_image/Sovrumsf%C3%B6rvaring%20fr%C3%A5n%20IKEA__bedroom_storage_340.jpg"
     }, {
         'name': "Klädförvaring",
         'link': "",
         'imgalt': "Beskrivning för kategorin",
-        'imgsrc': ""
+        'img': "http://www.ikea.com/se/sv/media/categories/seo_image/Sovrumsf%C3%B6rvaring%20fr%C3%A5n%20IKEA__bedroom_storage_340.jpg"
     }, {
         'name': "Dekoration",
         'link': "",
         'imgalt': "Beskrivning för kategorin",
-        'imgsrc': ""
+        'img': "http://www.ikea.com/se/sv/media/categories/seo_image/Sovrumsf%C3%B6rvaring%20fr%C3%A5n%20IKEA__bedroom_storage_340.jpg"
     }, {
         'name': "Golv",
         'link': "",
         'imgalt': "Beskrivning för kategorin",
-        'imgsrc': ""
+        'img': "http://www.ikea.com/se/sv/media/categories/seo_image/Sovrumsf%C3%B6rvaring%20fr%C3%A5n%20IKEA__bedroom_storage_340.jpg"
     }, {
         'name': "IKEA FAMILY produkter",
         'link': "",
         'imgalt': "Beskrivning för kategorin",
-        'imgsrc': ""
+        'img': "http://www.ikea.com/se/sv/media/categories/seo_image/Sovrumsf%C3%B6rvaring%20fr%C3%A5n%20IKEA__bedroom_storage_340.jpg"
     }, {
         'name': "Köksskåp & vitvaror",
         'link': "",
         'imgalt': "Beskrivning för kategorin",
-        'imgsrc': ""
+        'img': "http://www.ikea.com/se/sv/media/categories/seo_image/Sovrumsf%C3%B6rvaring%20fr%C3%A5n%20IKEA__bedroom_storage_340.jpg"
     }, {
         'name': "Belysning",
         'link': "",
         'imgalt': "Beskrivning för kategorin",
-        'imgsrc': ""
+        'img': "http://www.ikea.com/se/sv/media/categories/seo_image/Sovrumsf%C3%B6rvaring%20fr%C3%A5n%20IKEA__bedroom_storage_340.jpg"
     }, {
         'name': "Småförvaring",
         'link': "",
         'imgalt': "Beskrivning för kategorin",
-        'imgsrc': ""
+        'img': "http://www.ikea.com/se/sv/media/categories/seo_image/Sovrumsf%C3%B6rvaring%20fr%C3%A5n%20IKEA__bedroom_storage_340.jpg"
     }, {
         'name': "Soffor & fåtöljer",
         'link': "",
         'imgalt': "Beskrivning för kategorin",
-        'imgsrc': ""
+        'img': "http://www.ikea.com/se/sv/media/categories/seo_image/Sovrumsf%C3%B6rvaring%20fr%C3%A5n%20IKEA__bedroom_storage_340.jpg"
     }, {
         'name': "Förvaringsmöbler",
         'link': "",
         'imgalt': "Beskrivning för kategorin",
-        'imgsrc': ""
+        'img': "http://www.ikea.com/se/sv/media/categories/seo_image/Sovrumsf%C3%B6rvaring%20fr%C3%A5n%20IKEA__bedroom_storage_340.jpg"
     }, {
         'name': "Bord",
         'link': "",
         'imgalt': "Beskrivning för kategorin",
-        'imgsrc': ""
+        'img': "http://www.ikea.com/se/sv/media/categories/seo_image/Sovrumsf%C3%B6rvaring%20fr%C3%A5n%20IKEA__bedroom_storage_340.jpg"
     }, {
         'name': "Textil & Mattor",
         'link': "",
         'imgalt': "Beskrivning för kategorin",
-        'imgsrc': ""
+        'img': "http://www.ikea.com/se/sv/media/categories/seo_image/Sovrumsf%C3%B6rvaring%20fr%C3%A5n%20IKEA__bedroom_storage_340.jpg"
     }, {
         'name': "Verktyg & beslag",
         'link': "",
         'imgalt': "Beskrivning för kategorin",
-        'imgsrc': ""
+        'img': "http://www.ikea.com/se/sv/media/categories/seo_image/Sovrumsf%C3%B6rvaring%20fr%C3%A5n%20IKEA__bedroom_storage_340.jpg"
     }, {
         'name': "TV & mediaförvaring",
         'link': "",
         'imgalt': "Beskrivning för kategorin",
-        'imgsrc': ""
+        'img': "http://www.ikea.com/se/sv/media/categories/seo_image/Sovrumsf%C3%B6rvaring%20fr%C3%A5n%20IKEA__bedroom_storage_340.jpg"
     }];
 });
